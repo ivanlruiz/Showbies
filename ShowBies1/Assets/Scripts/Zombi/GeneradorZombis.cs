77ee4b013daa8bbaf97314031a085c8756d57590
ivanlruiz <ivancitolr@hotmail.com>
2022-10-22 19:24:57 -0300

    Nuevo Zombi y cosas

--- ShowBies1/Assets/Scripts/Zombi/GeneradorZombis.cs
+++ ShowBies1/Assets/Scripts/Zombi/GeneradorZombis.cs
@@ -8,23 +8,28 @@ public class GeneradorZombis : MonoBehaviour
     private GameObject Zombi;
     [SerializeField]
     private GameObject ZombiRapido;
+    [SerializeField]
+    private GameObject ZombiTanque;
 
     [SerializeField]
     private float intervaloZombi = 3.5f;
     [SerializeField]
     private float intervaloZombiRapido = 10f;
+    [SerializeField]
+    private float intervaloZombiTanque = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy(intervaloZombi, Zombi));
         StartCoroutine(spawnEnemy(intervaloZombiRapido, ZombiRapido));
+        StartCoroutine(spawnEnemy(intervaloZombiTanque, ZombiTanque));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-1f, 1), Random.Range(-2f, 2f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-10f, 10), Random.Range(-2f, 2f), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
